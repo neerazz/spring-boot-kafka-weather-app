809dc328bc082cad6bd142d05bc17404581b95c7
Neeraj Kumar Singh B <b.neerajkumarsingh@gmail.com>
2024-02-01 00:54:55 -0500

    Updated properties.

--- src/test/java/com/neeraj/poc/kafka/controller/HelloWorldControllerTest.java
+++ src/test/java/com/neeraj/poc/kafka/controller/HelloWorldControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -17,6 +19,7 @@ import java.io.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class HelloWorldControllerTest {
 
     @Autowired
--- src/test/java/com/neeraj/poc/kafka/controller/WeatherProducerControllerTest.java
+++ src/test/java/com/neeraj/poc/kafka/controller/WeatherProducerControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +21,7 @@ import java.io.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class WeatherProducerControllerTest {
 
 
